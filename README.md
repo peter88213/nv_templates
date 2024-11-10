7de7e79fb1773a7f790cccf5c5009872817c8d3d
peter88213 <petert88213@gmail.com>
2024-11-10 16:32:43 +0100

    Change API version to 5.0

--- README.md
+++ README.md
@@ -24,7 +24,7 @@ In *novelibre*, you can define a narrative structure with *stages* on two differ
 
 ## Requirements
 
-- [novelibre](https://github.com/peter88213/novelibre/) version 4.3+
+- [novelibre](https://github.com/peter88213/novelibre/) version 5.0+
 
 ## Download and install
 
--- src/nv_templates.py
+++ src/nv_templates.py
@@ -38,7 +38,7 @@ import tkinter as tk
 class Plugin(PluginBase):
     """A 'Story Templates' plugin class."""
     VERSION = '@release'
-    API_VERSION = '4.3'
+    API_VERSION = '5.0'
     DESCRIPTION = 'A "Story Templates" manager'
     URL = 'https://github.com/peter88213/nv_templates'
     _HELP_URL = f'https://peter88213.github.io/{_("nvhelp-en")}/nv_templates/'
