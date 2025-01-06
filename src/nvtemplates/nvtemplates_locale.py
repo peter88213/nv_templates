03aa9674bfc1f222a6e0d2cd2ed241f4598fc0fd
peter88213 <petert88213@gmail.com>
2025-01-06 23:07:28 +0100

    Update docstrings.

--- src/nvtemplates/nvtemplates_locale.py
+++ src/nvtemplates/nvtemplates_locale.py
@@ -1,6 +1,6 @@
 """Locale settings for nv_templates.
 
-Copyright (c) 2024 Peter Triesberger
+Copyright (c) 2025 Peter Triesberger
 For further information see https://github.com/peter88213/nv_templates
 License: GNU GPLv3 (https://www.gnu.org/licenses/gpl-3.0.en.html)
 """
